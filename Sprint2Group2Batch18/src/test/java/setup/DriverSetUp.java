80bdc9e12d86d5f800a419fb6365127078c76e59
Rajdeep <rajdeep3.9ghosh@gmail.com>
2025-07-24 23:16:14 +0530

    Create DriverSetup for browser launch and WebDriver management

--- Sprint2Group2Batch18/src/test/java/setup/DriverSetUp.java
+++ Sprint2Group2Batch18/src/test/java/setup/DriverSetUp.java
@@ -1,5 +1,13 @@
 package setup;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverSetUp {
+	static WebDriver driver;
+	public static WebDriver getDriver() {
+		driver=new ChromeDriver();
+		return driver;
+	}
 
 }
