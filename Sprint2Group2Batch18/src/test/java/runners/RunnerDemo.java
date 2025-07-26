b91e17749b3ed1c7e42d0ef06924eb3cd87c4ebf
karmakardebjyoti01 <debjyotiprofessional.net02@gmail.com>
2025-07-27 00:05:25 +0530

    committed classes

--- Sprint2Group2Batch18/src/test/java/runners/RunnerDemo.java
+++ Sprint2Group2Batch18/src/test/java/runners/RunnerDemo.java
@@ -3,18 +3,28 @@ package runners;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.*;
 
+//@RunWith(Cucumber.class)
+//@CucumberOptions( 
+//		features = {".\\src\\test\\java\\features\\FlightEnquiry.feature"},
+//		glue = "stepDefinitions",
+//		plugin = {
+//		        "pretty",
+//		        "html:reports/cucumber-reports/cucumber.html",
+//		        "json:reports/cucumber-reports/cucumber.json",
+//		        "junit:reports/cucumber-reports/cucumber.xml"
+//		    },
+//		    monochrome = true
+//)
+//public class RunnerDemo {
+////	plugin = {"pretty", "html:\\reports/reports.html"}
+//}
+
 @RunWith(Cucumber.class)
-@CucumberOptions( 
-		features = {".\\src\\test\\java\\features\\FlightEnquiry.feature"},
-		glue = "stepDefinitions",
-		plugin = {
-		        "pretty",
-		        "html:reports/cucumber-reports/cucumber.html",
-		        "json:reports/cucumber-reports/cucumber.json",
-		        "junit:reports/cucumber-reports/cucumber.xml"
-		    },
-		    monochrome = true
+@CucumberOptions(
+    features = "src/test/java/features/flightBooking",
+    glue = {"stepDefinitions", "hooks", "setup"},
+    plugin = {"pretty",  "junit:reports/junitReport.xml"},
+    monochrome = true,
+    dryRun = false
 )
-public class RunnerDemo {
-//	plugin = {"pretty", "html:\\reports/reports.html"}
-}
+public class RunnerDemo {}
